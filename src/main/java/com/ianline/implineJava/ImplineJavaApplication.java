66bda15dc9e6e39c701f5a48affe5d80fa2ad1e0
Iandi Santulus <iandisantulusn@gmail.com>
2020-12-05 15:19:22 +0700

    initial commit

---
+++ src/main/java/com/ianline/implineJava/ImplineJavaApplication.java
@@ -0,0 +1,13 @@
+package com.ianline.implineJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplineJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplineJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ianline/implineJava/ImplineJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ianline.implineJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplineJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
